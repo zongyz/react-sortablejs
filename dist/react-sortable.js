dd4f5d14fe962a1e290d2ef75b47dcdd6bee0a3e
Cheton Wu <cheton@gmail.com>
2016-04-11 11:01:58 +0800

    Prepare for v1

--- dist/react-sortable.js
+++ dist/react-sortable.js
@@ -60,10 +60,13 @@ return /******/ (function(modules) { // webpackBootstrap
 	Object.defineProperty(exports, "__esModule", {
 	    value: true
 	});
+	exports.default = undefined;
 
-	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+	var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
 
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	var _class, _temp2;
 
 	var _react = __webpack_require__(1);
 
@@ -79,161 +82,102 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+	function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-	var defaultOptions = {
-	    ref: 'list'
-	};
-
 	var store = {
 	    nextSibling: null,
 	    activeComponent: null
 	};
 
-	var refName = 'sortableComponent';
+	var _default = (_temp2 = _class = function (_React$Component) {
+	    _inherits(_default, _React$Component);
 
-	var extend = function extend(target) {
-	    for (var _len = arguments.length, sources = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-	        sources[_key - 1] = arguments[_key];
-	    }
+	    function _default() {
+	        var _Object$getPrototypeO;
 
-	    if (target === undefined || target === null) {
-	        throw new TypeError('Cannot convert undefined or null to object');
-	    }
+	        var _temp, _this, _ret;
 
-	    var output = Object(target);
-	    for (var index = 0; index < sources.length; index++) {
-	        var source = sources[index];
-	        if (source !== undefined && source !== null) {
-	            for (var key in source) {
-	                if (source.hasOwnProperty(key)) {
-	                    output[key] = source[key];
-	                }
-	            }
+	        _classCallCheck(this, _default);
+
+	        for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+	            args[_key] = arguments[_key];
 	        }
+
+	        return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_Object$getPrototypeO = Object.getPrototypeOf(_default)).call.apply(_Object$getPrototypeO, [this].concat(args))), _this), _this.sortable = null, _temp), _possibleConstructorReturn(_this, _ret);
 	    }
-	    return output;
-	};
 
-	var SortableMixin = function SortableMixin() {
-	    var options = arguments.length <= 0 || arguments[0] === undefined ? defaultOptions : arguments[0];
-	    return function (Component) {
-	        var _class, _temp2;
+	    _createClass(_default, [{
+	        key: 'componentDidMount',
+	        value: function componentDidMount() {
+	            var _this2 = this;
 
-	        return _temp2 = _class = function (_React$Component) {
-	            _inherits(_class, _React$Component);
+	            var _props = this.props;
+	            var children = _props.children;
+	            var className = _props.className;
 
-	            function _class() {
-	                var _Object$getPrototypeO;
+	            var options = _objectWithoutProperties(_props, ['children', 'className']);
 
-	                var _temp, _this, _ret;
+	            ['onStart', 'onEnd', 'onAdd', 'onSort', 'onUpdate', 'onRemove', 'onFilter', 'onMove'].forEach(function (name) {
+	                var eventHandler = options[name];
 
-	                _classCallCheck(this, _class);
+	                options[name] = function (evt) {
+	                    if (name === 'onStart') {
+	                        store.nextSibling = evt.item.nextElementSibling;
+	                        store.activeComponent = _this2;
+	                    } else if ((name === 'onAdd' || name === 'onUpdate') && _this2.props.onChange) {
+	                        var items = _this2.sortable.toArray();
+	                        var remote = store.activeComponent;
+	                        var remoteItems = remote.sortable.toArray();
 
-	                for (var _len2 = arguments.length, args = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
-	                    args[_key2] = arguments[_key2];
-	                }
+	                        evt.from.insertBefore(evt.item, store.nextSibling);
 
-	                return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_Object$getPrototypeO = Object.getPrototypeOf(_class)).call.apply(_Object$getPrototypeO, [this].concat(args))), _this), _this.state = {
-	                    sortableInstance: null
-	                }, _this.sortableOptions = extend({}, defaultOptions, options), _temp), _possibleConstructorReturn(_this, _ret);
-	            }
-
-	            _createClass(_class, [{
-	                key: 'componentDidMount',
-	                value: function componentDidMount() {
-	                    var _this2 = this;
-
-	                    [// Bind callbacks
-	                    'onStart', 'onEnd', 'onAdd', 'onSort', 'onUpdate', 'onRemove', 'onFilter', 'onMove'].forEach(function (name) {
-	                        _this2.sortableOptions[name] = function (evt) {
-	                            if (name === 'onStart') {
-	                                store.nextSibling = evt.item.nextElementSibling;
-	                                store.activeComponent = _this2;
-	                            } else if (name === 'onAdd' || name === 'onUpdate') {
-	                                evt.from.insertBefore(evt.item, store.nextSibling);
-
-	                                var oldIndex = evt.oldIndex;
-	                                var newIndex = evt.newIndex;
-	                                var items = _this2.props.items;
-	                                var remoteItems = [];
-
-	                                if (name === 'onAdd') {
-	                                    remoteItems = store.activeComponent.props.items;
-	                                    var item = remoteItems.splice(oldIndex, 1)[0];
-	                                    items.splice(newIndex, 0, item);
-	                                } else {
-	                                    items.splice(newIndex, 0, items.splice(oldIndex, 1)[0]);
-	                                }
-
-	                                // Called by any change to the list (add / update / remove)
-	                                _this2.props.onSort(items);
-
-	                                if (store.activeComponent !== _this2) {
-	                                    store.activeComponent.props.onSort(remoteItems);
-	                                }
+	                        if (remote !== _this2) {
+	                            if (_typeof(remote.props.group) === 'object' && remote.props.group.pull === 'clone') {
+	                                // Remove the node with the same data-reactid
+	                                evt.item.parentNode.removeChild(evt.item);
 	                            }
 
-	                            setTimeout(function () {
-	                                _this2.props[name] && _this2.props[name](evt, _this2.state.sortableInstance);
-	                            }, 0);
-	                        };
-	                    });
+	                            remote.props.onChange && remote.props.onChange(remoteItems, remote.sortable);
+	                        }
 
-	                    var sortableComponent = this.refs[refName];
-	                    this.initSortable(sortableComponent);
-	                }
-	            }, {
-	                key: 'componentDidUpdate',
-	                value: function componentDidUpdate(prevProps, prevState) {
-	                    var model = this.sortableOptions.model;
-	                    var prevItems = prevProps[model];
-	                    var currItems = this.props[model];
-	                    if (prevItems !== currItems) {
-	                        this.initSortable(this.refs[refName]);
+	                        _this2.props.onChange && _this2.props.onChange(items, _this2.sortable);
 	                    }
-	                }
-	            }, {
-	                key: 'componentWillUnmount',
-	                value: function componentWillUnmount() {
-	                    this.destroySortable();
-	                }
-	            }, {
-	                key: 'initSortable',
-	                value: function initSortable(sortableComponent) {
-	                    this.destroySortable();
-	                    var domNode = _reactDom2.default.findDOMNode(sortableComponent.refs[this.sortableOptions.ref] || sortableComponent);
-	                    var sortableInstance = _sortablejs2.default.create(domNode, this.sortableOptions);
-	                    this.setState({ sortableInstance: sortableInstance });
-	                }
-	            }, {
-	                key: 'destroySortable',
-	                value: function destroySortable() {
-	                    if (this.state.sortableInstance) {
-	                        this.state.sortableInstance.destroy();
-	                        this.setState({ sortableInstance: null });
-	                    }
-	                }
-	            }, {
-	                key: 'render',
-	                value: function render() {
-	                    return _react2.default.createElement(Component, _extends({ ref: refName }, this.props, this.state));
-	                }
-	            }]);
 
-	            return _class;
-	        }(_react2.default.Component), _class.propTypes = {
-	            items: _react2.default.PropTypes.array.isRequired,
-	            onSort: _react2.default.PropTypes.func.isRequired
-	        }, _temp2;
-	    };
-	};
+	                    setTimeout(function () {
+	                        eventHandler && eventHandler(evt);
+	                    }, 0);
+	                };
+	            });
 
-	exports.default = SortableMixin;
+	            this.sortable = _sortablejs2.default.create(_reactDom2.default.findDOMNode(this), options);
+	        }
+	    }, {
+	        key: 'render',
+	        value: function render() {
+	            var _props2 = this.props;
+	            var children = _props2.children;
+	            var className = _props2.className;
+	            var tag = _props2.tag;
+
+	            return _react2.default.DOM[tag]({ className: className }, children);
+	        }
+	    }]);
+
+	    return _default;
+	}(_react2.default.Component), _class.propTypes = {
+	    onChange: _react2.default.PropTypes.func,
+	    tag: _react2.default.PropTypes.string
+	}, _class.defaultProps = {
+	    tag: 'div'
+	}, _temp2);
+
+	exports.default = _default;
 
 /***/ },
 /* 1 */
