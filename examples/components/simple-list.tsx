c4811bfdcda9277fbf23067c20a64e495409afdd
Wayne Van Son <waynevspersonal@gmail.com>
2019-12-06 09:45:22 +1100

    renames basic list to simple list

---
+++ examples/components/simple-list.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { ReactSortable } from "../../src";
+import { Item, threes } from "./shared";
+
+export function SimpleList() {
+  const [list, setList] = useState(threes);
+  return (
+    <ReactSortable list={list} setList={setList} animation={150}>
+      {list.map(item => (
+        <Item key={item.id}>{item.name}</Item>
+      ))}
+    </ReactSortable>
+  );
+}
