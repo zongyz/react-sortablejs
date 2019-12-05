d54d189be183be9dc50c55eff4d746e4687778d9
Wayne Van Son <waynevspersonal@gmail.com>
2019-12-05 18:35:30 +1100

    creates css for examples page with grid

--- examples/app.tsx
+++ examples/app.tsx
@@ -6,13 +6,46 @@ const components = Object.entries(Examples);
 
 function Contents() {
   return (
-    <div>
-      {components.map(([name, Component]) => (
-        <Component key={name} />
+    <Wrapper>
+      {components.map(([name, Component], index) => (
+        <Container key={index}>
+          <Title>
+            {index} -  {name}
+          </Title>
+          <Component key={name} />
+        </Container>
       ))}
-    </div>
+    </Wrapper>
   );
 }
+const Title = styled.h4`
+  margin-bottom: 0.4rem;
+`;
+
+const Wrapper = styled.div`
+  display: grid;
+  padding: 2rem;
+  margin: 0 auto;
+  background-color: #eee;
+  max-width: 100rem;
+  gap: 20px;
+  @media (max-width: 900px) {
+    grid-template-columns: repeat(auto-fill, minmax(15rem, 1fr));
+  }
+  grid-template-columns: repeat(auto-fill, minmax(25rem, 1fr));
+`;
+
+const Container = styled.div`
+  border-radius: 0.2rem;
+  background-color: #ccc;
+  padding: 1rem;
+`;
+
+// handle
+// animations
+// different types of state
+// // lists, objects
+// callbacks
 
 export function App() {
   return (
