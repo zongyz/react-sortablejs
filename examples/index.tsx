140d441b62eaabccb3a11b42068bd3b84efcfc96
Wayne Van Son <waynevspersonal@gmail.com>
2019-12-06 09:44:57 +1100

    enable hot module reloading

--- examples/index.tsx
+++ examples/index.tsx
@@ -10,15 +10,16 @@ library.add(fas);
 const element = document.getElementById("app");
 if (!element) throw new Error("root element was not found");
 
-window.addEventListener("DOMContentLoaded", event => {
-  switch (process.env.NODE_ENV) {
-    case "development":
-      render(<App />, element);
-      break;
-    case "production":
-      hydrate(<App />, element);
-      break;
-    default:
-      throw new Error("Node environment not detected");
-  }
-});
+switch (process.env.NODE_ENV) {
+  case "development":
+    render(<App />, element);
+    break;
+  case "production":
+    hydrate(<App />, element);
+    break;
+  default:
+    throw new Error("Node environment not detected");
+}
+
+//@ts-ignore
+if (module.hot) module.hot.accept();
