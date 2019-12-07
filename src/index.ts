7e8f0a7e4375b8f98a13d7d2a1599db7a282088b
Wayne Van Son <waynevspersonal@gmail.com>
2019-12-07 17:57:38 +1100

    assigned exports to the component

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
 export { ReactSortable } from "./react-sortable";
 export * from "./types";
-export { MultiDrag, OnSpill, AutoScroll, Swap } from "sortablejs";
+export {
+  MultiDrag,
+  OnSpill,
+  AutoScroll,
+  Swap,
+  DOMRect,
+  Direction,
+  GroupOptions,
+  MoveEvent,
+  Options,
+  PullResult,
+  Plugin,
+  PutResult,
+  SortableEvent,
+  SortableOptions,
+  Utils
+} from "sortablejs";
+
+import SortableGlobal from "sortablejs";
+export const Sortable = SortableGlobal;
