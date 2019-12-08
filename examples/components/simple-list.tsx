d2b582c43c1a744897e5881b89aa2181337708a6
Wayne Van Son <waynevspersonal@gmail.com>
2019-12-08 14:08:27 +1100

    add element.id to react sortable

--- examples/components/simple-list.tsx
+++ examples/components/simple-list.tsx
@@ -5,10 +5,18 @@ import { Item, threes } from "../util";
 export function SimpleList() {
   const [list, setList] = useState(threes);
   return (
-    <ReactSortable list={list} setList={setList} animation={150}>
-      {list.map(item => (
-        <Item key={item.id}>{item.name}</Item>
+    <ReactSortable
+      id="simple-list"
+      list={list}
+      setList={setList}
+      animation={150}
+    >
+      {list.map((item, index) => (
+        <Item className="test" id={`simple-item-${item.id}`} key={item.id}>
+          {item.name}
+        </Item>
       ))}
     </ReactSortable>
   );
 }
+  
