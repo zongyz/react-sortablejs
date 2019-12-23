29ad5c1bc7db8ee3ceed95b7ace47a07ef1a2535
Wayne Van Son <waynevspersonal@gmail.com>
2019-12-23 16:56:23 +1100

    [util] create cretae custom function

--- src/util.ts
+++ src/util.ts
@@ -34,10 +34,63 @@ export function insertNodeAt(
 export function handleDOMChanges<T extends ItemInterface>(
   customs: Normalized<T>[]
 ) {
+  removeNodes(customs);
+  insertNodes(customs);
+}
+
+export function removeNodes<T extends ItemInterface>(customs: Normalized<T>[]) {
   customs.forEach(curr => removeNode(curr.element));
+}
+
+export function insertNodes<T extends ItemInterface>(customs: Normalized<T>[]) {
   customs.forEach(curr => {
     insertNodeAt(curr.parentElement, curr.element, curr.oldIndex);
   });
+}
+
+export function createCustoms<T extends ItemInterface>(
+  evt: MultiDragEvent,
+  list: T[]
+) {
+  const mode = getMode(evt);
+  const parentElement = { parentElement: evt.from };
+  let custom = [];
+  switch (mode) {
+    case "normal":
+      const item = {
+        element: evt.item,
+        newIndex: evt.newIndex!,
+        oldIndex: evt.oldIndex!,
+        parentElement: evt.from
+      };
+      custom = [item];
+      break;
+    case "swap":
+      const drag: Input = {
+        element: evt.item,
+        oldIndex: evt.oldIndex!,
+        newIndex: evt.newIndex!,
+        ...parentElement
+      };
+      const swap: Input = {
+        element: evt.swapItem!,
+        oldIndex: evt.newIndex!,
+        newIndex: evt.oldIndex!,
+        ...parentElement
+      };
+      custom = [drag, swap];
+      break;
+    case "multidrag":
+      custom = evt.oldIndicies.map<Input>((curr, index) => ({
+        element: curr.multiDragElement,
+        oldIndex: curr.index,
+        newIndex: evt.newIndicies[index].index,
+        ...parentElement
+      }));
+      break;
+  }
+  const customs = createNormalized(custom, list);
+  return customs;
 }
 
 /** moves items form old index to new index without breaking anything ideally. */
