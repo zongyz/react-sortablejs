9db7297ecb2985122dc431a6df869833d440d861
Wayne Van Son <waynevanson@gmail.com>
2020-09-25 23:32:50 +0000

    fix(util.ts): changes custom type Omit to extend keyof T

--- src/util.ts
+++ src/util.ts
@@ -197,4 +197,4 @@ export function destructurePropsForOptions<T>(
  * Construct a type with the properties of T except for those in type K.
  * Including this allows for backwards compatibility with earlier versions of TS.
  */
-export type Omit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
