f3f726d8288ad269a01e4d52ef9ce42e7f95228f
Wayne Van Son <waynevanson@gmail.com>
2020-09-25 14:35:01 +0000

    chore(dist): removes and ignores dist files from git

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as Sortable, Direction, DOMRect, GroupOptions, MoveEvent, MultiDrag, Options, PullResult, PutResult, SortableEvent, SortableOptions, Swap, Utils } from "sortablejs";
-export { ReactSortable } from "./react-sortable";
-export * from "./types";
-//# sourceMappingURL=index.d.ts.map
